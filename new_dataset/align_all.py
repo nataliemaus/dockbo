060cdd4c5fc4b25c7e079d9417eb991678d7ea19
nataliemaus <natalietmaus@gmail.com>
2022-12-08 13:24:19 -0500

    align and evaluate

--- new_dataset/align_all.py
+++ new_dataset/align_all.py
@@ -1,20 +1,58 @@
 import pymolPy3
+import sys 
+sys.path.append("../")
+# from dockbo.utils.fold_utils.fold_utils import remove_hetero_atoms_and_hydrogens
 
-
-def align(pm, known_pose, new_ab_structure):
-    pm = pymolPy3.pymolPy3(0)
-    pm('delete all')
-    # Load everything 
-    pm(f'load {known_pose}.pdb')
-    pm(f'load {new_ab_structure}.pdb')
-    pm(f'super {new_ab_structure}, {known_pose}')
-    pm(f'select old_ab, model {known_pose} and chain XXX') # you'll have to figure out which chain is which 
-    pm(f'remove old_ab') 
-    pm('save new_structure.pdb')
+def align(pm, known_pose_id, new_ab_structure_path, new_ab_structure):
+    assert known_pose_id in [1,2]
+    known_pose_path=f'1cz8_known_poses/known_ab_pose{known_pose_id}'
+    known_pose = f"known_ab_pose{known_pose_id}"
     pm('delete all') 
+    pm(f'load {known_pose_path}.pdb')
+    pm(f'load {new_ab_structure_path}.pdb')
+    pm(f'super {new_ab_structure}, {known_pose}')
+    pm(f'select old_ab, model {known_pose}') # you'll have to figure out which chain is which 
+    pm(f'remove old_ab') 
+    pm(f"save {new_ab_structure_path}_aligned{known_pose_id}.pdb") 
+    pm(f'delete all') 
 
-align(
-    pm=pm, 
-    known_pose='1cz8_known_poses/known_ab_pose1', 
-    new_ab_structure=save_path.replace(".pdb", "")
-)
+
+def align_all(n_seqs=100_000):
+    pm = pymolPy3.pymolPy3(0)
+    for seq_id in range(n_seqs):
+        try: 
+            new_path = f"folded_pdbs/seq{seq_id}"
+            ab_name = f"seq{seq_id}"
+            align(
+                pm=pm, 
+                known_pose_id=1,
+                new_ab_structure_path=new_path, # save_path.replace(".pdb", "")
+                new_ab_structure=ab_name, # save_path.replace(".pdb", "")
+            )
+            # remove_hetero_atoms_and_hydrogens(f"{new_path}_aligned1.pdb") 
+            align(
+                pm=pm, 
+                known_pose_id=2,
+                new_ab_structure_path=new_path, # save_path.replace(".pdb", "")
+                new_ab_structure=ab_name, # save_path.replace(".pdb", "")
+            )
+            # remove_hetero_atoms_and_hydrogens(f"{new_path}_aligned2.pdb") 
+            # align(
+            #     pm=pm, 
+            #     known_pose_id=1,
+            #     new_ab_structure_path=f"folded_pdbs/seq{seq_id}_no_het_noh", # save_path.replace(".pdb", "")
+            #     new_ab_structure=f"seq{seq_id}_no_het_noh", # save_path.replace(".pdb", "")
+            # )
+            # align(
+            #     pm=pm, 
+            #     known_pose_id=2,
+            #     new_ab_structure_path=f"folded_pdbs/seq{seq_id}_no_het_noh", # save_path.replace(".pdb", "")
+            #     new_ab_structure=f"seq{seq_id}_no_het_noh", # save_path.replace(".pdb", "")
+            # )
+            
+        except:
+            pass 
+
+# conda activate pymol 
+# python3 align_all.py
+align_all(n_seqs = 2)
---
+++ new_dataset/compute_scores.py
@@ -0,0 +1,72 @@
+
+import sys 
+sys.path.append("../")
+from dockbo.dockbo import DockBO
+import numpy as np
+import pandas as pd
+from dockbo.utils.fold_utils.fold_utils import remove_hetero_atoms_and_hydrogens
+
+def compute_scores(args):
+    antigen_path = args.work_dir + 'dockbo/new_dataset/1cz8_known_poses/known_ag_pose.pdb'
+    true_ab_path = args.work_dir + f'dockbo/new_dataset/1cz8_known_poses/known_ab_pose{args.known_pose_id}.pdb'
+    oracle = DockBO( 
+        path_to_default_antigen_pdb=antigen_path,
+        path_to_default_antigen_restraints=None,
+        work_dir=args.work_dir, 
+        scoring_func=args.score_f, # 'dfire', 'dfire2'
+        verbose_config_opt=True,
+        negate_difre2=False, 
+        init_bo_w_known_pose=True, 
+    )
+    seq_ids = []
+    energies = [] 
+    for seq_id in range(args.n_seqs):
+        aligned_ab_structure_path= args.work_dir + f"dockbo/new_dataset/folded_pdbs/seq{seq_id}_aligned{args.known_pose_id}.pdb"
+        try:
+            aligned_ab_path = remove_hetero_atoms_and_hydrogens(aligned_ab_structure_path)
+            if seq_id == 0:
+                true_dict = oracle(
+                    config_x="default",
+                    path_to_antibody_pdb=true_ab_path,
+                    save_pdb_pose_path=true_ab_path.replace(".pdb", "_combined_structure")
+                )
+                energy = true_dict['energy']
+                seq_ids.append(-1) # -1 indicates the known strucutre! 
+                energies.append(energy)
+            return_dict = oracle(
+                config_x="default", 
+                path_to_antibody_pdb=aligned_ab_path,
+                save_pdb_pose_path=aligned_ab_path.replace(".pdb", "_combined_structure")
+            )
+            energy = return_dict['energy']
+            seq_ids.append(seq_id)
+            energies.append(energy)
+        except Exception as e: 
+            if args.debug:
+                print(e)  
+                import pdb 
+                pdb.set_trace() 
+    # save all computed seq ids and energies 
+    seq_ids = np.array(seq_ids )
+    energies = np.array(energies )
+    df = pd.DataFrame() 
+    df['seq_id'] = seq_ids
+    df[f"{args.score_f}_score"] = energies 
+    df.to_csv(args.work_dir + f"dockbo/new_dataset/{args.score_f}_scores.csv", index=None)
+
+if __name__ == "__main__": 
+    import argparse 
+    parser = argparse.ArgumentParser()  
+    parser.add_argument('--score_f', default="dfire" ) # "dfire, cpydock, dfire2"
+    parser.add_argument('--work_dir', default='/home/nmaus/' ) 
+    parser.add_argument('--known_pose_id', type=int, default=1 ) 
+    parser.add_argument('--n_seqs', type=int, default=100_000 ) 
+    parser.add_argument('--debug', type=bool, default=False ) 
+    args = parser.parse_args() 
+    if args.debug: 
+        args.n_seqs = 2 
+    compute_scores(args) 
+    #  conda activate og_lolbo_mols 
+    # python3 compute_scores.py --debug True 
+
+
